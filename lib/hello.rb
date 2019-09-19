704a6ac944c1aa85d81b00d3c73f97b9a78cbf89
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-19 20:04:57 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array)
   # set a counter variable, i, equal to 0
   i = 0 
   
-  while i < array.length
+  while i < array.length #execute as long as i < length of array
     i += 1
   end
   
