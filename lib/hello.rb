d2c0cec210006a62891f193396711a49777530c8
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-19 20:07:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,6 +3,9 @@ def hello_t(array)
   i = 0 
   
   while i < array.length #execute as long as i < length of array
+    # i = a given index number of our array in each iteration
+    # use this info to yield each successive value stored in the array 
+    # to the passed-in block:
     yield array[i]
     i += 1 # increment the value of i
   end
