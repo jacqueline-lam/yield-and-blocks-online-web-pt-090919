0e3c6201ea2a1c8eeaf4634cd1a3109306d16295
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-19 20:27:23 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,6 @@
 def hello_t(array)
   # set a counter variable, i, equal to 0
-  if block_given? 
+  if block_given? #returns true if the method is called with a block 
     i = 0 
   
     while i < array.length #execute as long as i < length of array
