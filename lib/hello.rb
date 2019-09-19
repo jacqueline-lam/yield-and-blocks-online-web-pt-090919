2f42fdec95bafe138fc21d2ec4afe073d11a5a59
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-19 20:27:03 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,19 +1,25 @@
 def hello_t(array)
   # set a counter variable, i, equal to 0
-  i = 0 
+  if block_given? 
+    i = 0 
   
-  while i < array.length #execute as long as i < length of array
-    # i = a given index number of our array in each iteration
-    # use this info to yield each successive value stored in the array 
-    # to the passed-in block:
-    yield array[i]
-    # [] method to grab the value of each successive index element as we proceed through our while loop
-    # yielding each to a block
-    i += 1 # increment the value of i
+    while i < array.length #execute as long as i < length of array
+      # i = a given index number of our array in each iteration
+      # use this info to yield each successive value stored in the array 
+      # to the passed-in block:
+      yield array[i]
+      # [] method to grab the value of each successive index element as we proceed through our while loop
+      # yielding each to a block
+      i += 1 # increment the value of i
+    end
+  
+    array
+  else
+    puts "Hey! No block was given!"
   end
-  
-  array
 end
+
+#method can be called either with or without a block
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
